a868a42a96a4cce0d9e1afec5472e28afeebfd77
HyungUk Ryu <hw0603@naver.com>
2024-07-22 17:56:14 +0900

    dd

--- backend/src/main/java/com/woowacourse/momo/controller/meeting/MeetingController.java
+++ backend/src/main/java/com/woowacourse/momo/controller/meeting/MeetingController.java
@@ -4,6 +4,7 @@ import com.woowacourse.momo.controller.MomoApiResponse;
 import com.woowacourse.momo.service.meeting.MeetingService;
 import com.woowacourse.momo.service.meeting.dto.MeetingResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class MeetingController {
+
+    @Value("${test.helloworld}")
+    private String fromYaml;
 
     private final MeetingService meetingService;
 
@@ -22,6 +26,6 @@ public class MeetingController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World!";
+        return fromYaml;
     }
 }
